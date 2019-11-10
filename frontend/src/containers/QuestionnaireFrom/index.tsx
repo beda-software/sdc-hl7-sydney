813ceb0b221dc50c8deec45d654cd5e76bdb4485
Dmitry Shutov <dmitryashutov@gmail.com>
2019-11-10 17:30:27 +0700

    Questionnaire create

--- frontend/src/containers/QuestionnaireFrom/index.tsx
+++ frontend/src/containers/QuestionnaireFrom/index.tsx
@@ -3,17 +3,27 @@ import { Form, Input, Button, Row, Col, Select } from 'antd';
 import { Form as FinalFrom, Field } from 'react-final-form';
 import arrayMutators from 'final-form-arrays'
 import { useFieldArray } from 'react-final-form-arrays';
-import { RemoteData } from 'src/contrib/aidbox-react/libs/remoteData';
+// import { RemoteData } from 'src/contrib/aidbox-react/libs/remoteData';
 import { Questionnaire, QuestionnaireItem } from 'src/contrib/aidbox';
 import _ from 'lodash';
+import { saveFHIRResource } from '../../contrib/aidbox-react/services/fhir';
 
-function onSubmit(): RemoteData<Questionnaire> {
-    return {} as RemoteData<Questionnaire>
+async function onSubmit(data: any) {
+    const questionnaire: Questionnaire = data;
+    _.each(questionnaire.item, (item) => {
+        item.linkId = _.replace(item.text!, /\s+/g, '-')
+    })
+
+    return await saveFHIRResource(questionnaire)
 }
 
 interface InputFieldProps {
     name: string;
     label: string;
+}
+
+interface QuestionnaireFormProps {
+    questionnaire?: Questionnaire;
 }
 
 type SelectFieldProps = InputFieldProps & { options: string[] };
@@ -77,10 +87,10 @@ function Questions() {
           return (
             <Row key={index} gutter={4}>
               <Col span={11}>
-                <InputField name="text" label="Question description" />
+                <InputField name={`${name}.text`} label="Question description" />
               </Col>
               <Col span={11}>
-                <SelectField name="type" label="Question type" options={["string", "integer"]} />
+                <SelectField name={`${name}.type`} label="Question type" options={["string", "integer"]} />
               </Col>
               <Col span={2}>
                 <Button
@@ -96,16 +106,20 @@ function Questions() {
     );
 }
 
-export function QuestionnaireFrom() {
+export function QuestionnaireForm(props: QuestionnaireFormProps) {
     return (
         <FinalFrom
             onSubmit={onSubmit}
+            initialValues={{...props.questionnaire, resourceType: 'Questionnaire', status: 'active'}}
             mutators={{...arrayMutators}}
             render={({ handleSubmit, form: { mutators: { push, pop } }, values }) => {
                 return (
                     <Form onSubmit={handleSubmit}>
                         <InputField name="title" label="Questionnaire title"/>
                         <Questions/>
+                        <Button type="primary" htmlType="submit">
+                            Save
+                        </Button>
                     </Form>
                 );
             }}
