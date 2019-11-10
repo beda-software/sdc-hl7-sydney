346e7a211260e3bf9c5f1aa7fd4da51be97a6f94
Ilya Beda <ir4y.ix@gmail.com>
2019-11-10 14:42:51 +0300

    debug deploy script

--- chart/deploy.sh
+++ chart/deploy.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh -x
 mkdir -p /root/.kube/
 echo "$K8S_CONFIG" > /root/.kube/config
 cd /src
@@ -6,4 +6,4 @@ kubectl config use-context aidbox-nova-beda
 kubectl delete secret  beda-software-registry-rgv-api
 # Since pull is async I don't know how to use build token for it
 kubectl create secret docker-registry  beda-software-registry-rgv --docker-server=$CI_REGISTRY --docker-username=gitlab+deploy-token-11 --docker-password=rhXGWQsuzD9kKWEssy7c
-helm upgrade --set backend.image.tag=master --set backend.image.hash=$1  --set frontend.image.hash=$1 --namespace=bedasoftware -i rgv .
+helm upgrade --set backend.image.hash=$1  --set frontend.image.hash=$1 --namespace=bedasoftware -i rgv .
