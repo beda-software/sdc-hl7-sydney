a4a30c3d7eee2e7c09988149232ccc8d0047349d
alex <pavlushkin87@gmail.com>
2019-11-10 21:41:21 +0700

    Fix table x scroll

--- frontend/src/containers/QuestionnaireResponseVIew/index.tsx
+++ frontend/src/containers/QuestionnaireResponseVIew/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef }  from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import * as _ from 'lodash';
 import { Spin, Table, Input, Button, Icon } from 'antd';
 import * as ReactRouter from 'react-router';
@@ -38,10 +38,10 @@ export function useQuestionnaireResponseQuery(
 
 export function QuestionnairesResponseView(props: Props) {
     const [questionnaire] = useService<Questionnaire>(async () => {
-        return getFHIRResource<Questionnaire>({resourceType: 'Questionnaire', id: props.match.params.id});
+        return getFHIRResource<Questionnaire>({ resourceType: 'Questionnaire', id: props.match.params.id });
     })
-    if(isSuccess(questionnaire)) {
-        return <QuestionnaireResponseTable questionnaire={questionnaire.data}/>;
+    if (isSuccess(questionnaire)) {
+        return <QuestionnaireResponseTable questionnaire={questionnaire.data} />;
 
     }
     return <Spin />
@@ -51,56 +51,56 @@ interface QuestionnaireResponseTableProps {
 
 }
 function QuestionnaireResponseTable({ questionnaire }: QuestionnaireResponseTableProps) {
-   const [params, setParams] = useState<any>({ query: questionnaire['id'] });
+    const [params, setParams] = useState<any>({ query: questionnaire['id'] });
 
     const questionnaireResponse = useQuestionnaireResponseQuery(params);
 
     const searchInput = useRef<Input>(null);
 
-    function handleSearch(linkId:string, keys:any, confirm:any){
+    function handleSearch(linkId: string, keys: any, confirm: any) {
         confirm();
-        setParams({...params, [linkId]: keys});
+        setParams({ ...params, [linkId]: keys });
     }
 
-    function handleReset(linkId:string, clearFilters:any) {
+    function handleReset(linkId: string, clearFilters: any) {
         clearFilters();
         setParams(_.omit(params, [linkId]))
     }
 
-    const getColumnSearchProps = (dataIndex:string) => ({
-        filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }:any) => (
-          <div style={{ padding: 8 }}>
-            <Input
-              ref={searchInput}
-              placeholder={`Search ${dataIndex}`}
-              value={selectedKeys[0]}
-              onChange={e => setSelectedKeys(e.target.value ? [e.target.value] : [])}
-              onPressEnter={() => handleSearch(dataIndex, selectedKeys, confirm)}
-              style={{ width: 188, marginBottom: 8, display: 'block' }}
-            />
-            <Button
-              type="primary"
-              onClick={() => handleSearch(dataIndex, selectedKeys, confirm)}
-              icon="search"
-              size="small"
-              style={{ width: 90, marginRight: 8 }}
-            >
-              Search
+    const getColumnSearchProps = (dataIndex: string) => ({
+        filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }: any) => (
+            <div style={{ padding: 8 }}>
+                <Input
+                    ref={searchInput}
+                    placeholder={`Search ${dataIndex}`}
+                    value={selectedKeys[0]}
+                    onChange={e => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+                    onPressEnter={() => handleSearch(dataIndex, selectedKeys, confirm)}
+                    style={{ width: 188, marginBottom: 8, display: 'block' }}
+                />
+                <Button
+                    type="primary"
+                    onClick={() => handleSearch(dataIndex, selectedKeys, confirm)}
+                    icon="search"
+                    size="small"
+                    style={{ width: 90, marginRight: 8 }}
+                >
+                    Search
             </Button>
-            <Button onClick={() => handleReset(dataIndex, clearFilters)} size="small" style={{ width: 90 }}>
-              Reset
+                <Button onClick={() => handleReset(dataIndex, clearFilters)} size="small" style={{ width: 90 }}>
+                    Reset
             </Button>
-          </div>
+            </div>
         ),
-        filterIcon: (filtered:any) => (
-          <Icon type="search" style={{ color: filtered ? '#1890ff' : undefined }} />
+        filterIcon: (filtered: any) => (
+            <Icon type="search" style={{ color: filtered ? '#1890ff' : undefined }} />
         ),
-        onFilterDropdownVisibleChange: (visible:boolean) => {
-          if (visible) {
-            setTimeout(() => searchInput.current!.select());
-          }
+        onFilterDropdownVisibleChange: (visible: boolean) => {
+            if (visible) {
+                setTimeout(() => searchInput.current!.select());
+            }
         },
-      });
+    });
 
     const questionTypes = {};
 
@@ -111,7 +111,7 @@ function QuestionnaireResponseTable({ questionnaire }: QuestionnaireResponseTabl
             dataIndex: item.linkId,
             key: item.linkId,
         }
-        if(item.type === "string" || item.type === "integer") {
+        if (item.type === "string" || item.type === "integer") {
             column = {
                 ...column,
                 ...getColumnSearchProps(item.linkId)
@@ -125,17 +125,22 @@ function QuestionnaireResponseTable({ questionnaire }: QuestionnaireResponseTabl
         const dataSource = _.map(questionnaireResponse.data.entry, ({ resource }: { resource: QuestionnaireResponse }) => {
             const data = { key: resource.id, };
             _.each(resource.item, (item) => {
-                const type = questionTypes[item.linkId];
-                if (type === 'choice') {
-                    data[item.linkId] = item.answer![0].value!.Coding!.display!;
-                } else {
-                    data[item.linkId] = item.answer![0].value![type];
+                try {
+                    const type = questionTypes[item.linkId];
+                    if (type === 'choice') {
+                        data[item.linkId] = item.answer![0].value!.Coding!.display!;
+                    } else {
+                        data[item.linkId] = item.answer![0].value![type];
+                    }
+                }
+                catch {
+                    data[item.linkId] = 'null'
                 }
             });
             return data;
         });
 
-        return <Table columns={columns} dataSource={dataSource} />;
+        return <Table columns={columns} dataSource={dataSource} scroll={{ x: true }} />;
     }
 
     return <Spin />
--- frontend/src/containers/QuestionnairesList/index.tsx
+++ frontend/src/containers/QuestionnairesList/index.tsx
@@ -15,38 +15,40 @@ export function QuestionnairesList() {
 
         return (
             <>
-                <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-                    <h2>Questionnaires</h2>
-                    <Link to='/questionnaire/new'>
-                        <Button type='primary'>CreateQuestionnaire</Button>
-                    </Link>
+                <div style={{ maxWidth: '900px' }}>
+                    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                        <h2>Questionnaires</h2>
+                        <Link to='/questionnaire/new'>
+                            <Button type='primary'>CreateQuestionnaire</Button>
+                        </Link>
+                    </div>
+
+                    <hr />
+                    <List<Questionnaire>
+                        dataSource={questionnaires}
+                        renderItem={(questionnaire) => (
+                            <List.Item
+                                key={questionnaire.id}
+
+                            >
+                                <List.Item.Meta title={questionnaire.title} />
+                                <div>
+                                    <Link to={`/questionnaire/edit/${questionnaire.id}`}>
+                                        <Button type='default' size='small'>Edit questionnaire</Button>
+                                    </Link>
+                                    {' '}
+                                    <Link to={`/questionnaire/fill/${questionnaire.id}`}>
+                                        <Button type='default' size='small'>Add response</Button>
+                                    </Link>
+                                    {' '}
+                                    <Link to={`/questionnaire-response/${questionnaire.id}`}>
+                                        <Button type='default' size='small'>View responses</Button>
+                                    </Link>
+                                </div>
+                            </List.Item>
+                        )}
+                    />
                 </div>
-
-                <hr />
-                <List<Questionnaire>
-                    dataSource={questionnaires}
-                    renderItem={(questionnaire) => (
-                        <List.Item
-                            key={questionnaire.id}
-
-                        >
-                            <List.Item.Meta title={questionnaire.title} />
-                            <div>
-                                <Link to={`/questionnaire/edit/${questionnaire.id}`}>
-                                    <Button type='default' size='small'>Edit questionnaire</Button>
-                                </Link>
-                                {' '}
-                                <Link to={`/questionnaire/fill/${questionnaire.id}`}>
-                                    <Button type='default' size='small'>Add response</Button>
-                                </Link>
-                                {' '}
-                                <Link to={`/questionnaire-response/${questionnaire.id}`}>
-                                    <Button type='default' size='small'>View responses</Button>
-                                </Link>
-                            </div>
-                        </List.Item>
-                    )}
-                />
             </>
         );
     }
